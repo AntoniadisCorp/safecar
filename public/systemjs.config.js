4590094234dfef9fad99469ba31b619bf54bf510
prokopis3 <prokopis123@gmail.com>
2017-07-30 13:50:37 +0300

    node packages upgraded

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -13,23 +13,27 @@
       // our app is within the app folder
       app: 'app',
 
-      // angular bundles
+       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
-      // other libraries
+      '@angular/material':         'npm:@angular/material/bundles/material.umd.js',
+      '@angular/cdk':              'npm:@angular/cdk/bundles/cdk.umd.js',
+      '@angular/animations':         'npm:@angular/animations/bundles/animations.umd.js',
+       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
       'angular2-fontawesome':      'npm:angular2-fontawesome',
-      '@angular/material':         'npm:@angular/material/bundles/material.umd.js',
       'hammerjs':                  'npm:hammerjs/hammer.js',
+
       // further angular bundles
     },
     // packages tells the System loader how to load when no filename and/or no extension
